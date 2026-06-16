e507112c103a5e5f7bd045b2005263991c1854d4
Arjun Tomar <arjun16tomar@gmail.com>
2026-06-16 14:51:13 +0530

    Add UnansweredQueries model and create serializer and urls files

--- backend/chatbot/models.py
+++ backend/chatbot/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core.models import BaseModel, Client
 
-# Create your models here.
+class UnansweredQueries(BaseModel):
+    client_id = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        verbose_name='client',
+        editable=False
+    )
+
+    query = models.TextField(blank=False, null=False)
+
+    is_resolved = models.BooleanField(default=False, db_index=True)
+
+    class Meta:
+        ordering = ['-created_at', 'is_resolved']
+        verbose_name = 'Unanswered Query'
+        verbose_name_plural = 'Unanswered Queries'
